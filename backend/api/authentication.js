4eca4b2737856a0a799564fe5b1499b4285abd06
Erik Davtyan <erikdavtyan@gmail.com>
2017-07-13 16:42:06 +0400

    inital commit

---
+++ backend/api/authentication.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+const User = mongoose.model('User');
+
+router
+	.post('/signup', async (req, res) => {
+		const { username, password } = req.body;
+		const user = await User.findOne({ username });
+		if (user) {
+			res.send({
+				success: false,
+				message: 'Username is already taken'
+			});
+		}
+		else {
+			passHash = await bcrypt.hash(password, 10);
+			const account = new User({
+				username,
+				password: passHash
+			});
+			await account.save()
+			res.json({ success: 'true', message: 'Please log in' });
+		}
+	})
+	.post('/login', async (req, res) => {
+		const { username, password } = req.body;
+		const user = await User.findOne({ username });
+		if (!user || !(await bcrypt.compare(password, user.password))) {
+			res.json({ success: false, message: 'Username or password is incorrect' });
+		} else {
+			jwt.sign(username, process.env.SECRET, { algorithm: 'HS512'}, (e, token) => {
+				res.json({ success: true, message: 'Token generated!', token });
+			})
+		}
+	})
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+require('./models/user');
+require('dotenv').config();
+
+const app = express();
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB);
+
+const PORT = process.env.PORT;
+
+app
+	.use(bodyParser.urlencoded({ extended: true }))
+	.use(bodyParser.json())
+	.use('/auth', require('./api/authentication'))
+	.use((req, res, next) => {
+		const { token } = req.body;
+		if (!token) {
+			res.status(403).send({
+				success: false,
+				message: 'Please login'
+			})
+		} else {
+			jwt.verify(token, process.env.SECRET, { algorithms: 'HS512' }, (e, decoded) => {
+				if (e) {
+					res.json({ success: false, message: 'Failed to authenticate token, please log in again'})
+				} else {
+					req.username = decoded;
+					// username is available in middlewares below
+					res.json({ message: 'Authenticated' })
+				}
+			})
+		}
+	})
+	.listen(PORT, () => {
+		console.log(`Listening on port ${PORT}`)
+	});
